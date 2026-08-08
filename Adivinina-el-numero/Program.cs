f8ea3778af4eefc30e51a9beccc6ee20345519c4
angel <angel@G0DZILLA>
2026-08-08 17:23:10 -0500

    Agregar archivos de proyecto.

---
+++ Adivinina-el-numero/Program.cs
@@ -0,0 +1,27 @@
+﻿Random numeroAleatorio = new Random();
+
+int numeroCualquiera = numeroAleatorio.Next(0, 100);
+int numeroIntentos = 0;
+
+while (numeroCualquiera <= 100)
+{
+    Console.WriteLine("Digite el numero para saber si acerto el numero cualquiera: ");
+    int numero = int.Parse(Console.ReadLine());
+
+    if (numero > numeroCualquiera)
+    {
+        Console.WriteLine("Su numero es mayor al numero cualquiera");
+        numeroIntentos++;
+    }else if (numero < numeroCualquiera)
+    {
+        Console.WriteLine("Su numero es menor al numero cualquiera");
+        numeroIntentos++;
+    }
+    else
+    {
+        Console.WriteLine("Felicitaciones, has acertado el numero");
+        break;
+    }
+}
+
+Console.WriteLine($"Numero de intentos {numeroIntentos}");
